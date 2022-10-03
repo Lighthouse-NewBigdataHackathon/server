11b3bda38d5bca5b08c1186bedc65ad4f04aebf5
Hong <fheldgktpdy@khu.ac.kr>
2022-10-04 01:01:52 +0900

    update routes,main

--- routes.py
+++ routes.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -6,5 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('main.html')
 
+
+@app.route('/articles', methods=['GET'])
+def today():
+    if request.method == 'GET':   #데이터를 받아야 아래 코드를 더 짤수 있을듯 하네용.. 우선은 articles로 넘어가짐
+        return render_template("articles.html")
+
+
+    
 if __name__ == '__main__':
     app.run()
