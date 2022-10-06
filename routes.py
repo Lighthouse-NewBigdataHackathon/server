bc87bc0c8e25e442ad6926da08e3f205fa82ec32
dhk1349 <dhk1349@gmail.com>
2022-10-07 04:02:20 +0900

    .

--- routes.py
+++ routes.py
@@ -1,7 +1,14 @@
 import re
+import pymysql
+import db
+import pickle
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+with open("../db_data.pickle", 'rb') as f:
+    db_data = pickle.load(f)
+conn, cursor = db.connect_RDS(db_data["host"], db_data["port"], db_data["username"], db_data["password"], db_data["database"])
 
 @app.route('/')
 def home():
@@ -11,9 +18,14 @@ def home():
 @app.route('/articles', methods=['GET'])
 def today():
     if request.method == 'GET':   #데이터를 받아야 아래 코드를 더 짤수 있을듯 하네용.. 우선은 articles로 넘어가짐
-        return render_template("articles.html")
+        date='2022-09-30'
+        q = db.select_with_date(date)
+        cursor.execute(q)
+        result = cursor.fetchall()
+        # print(result)
+        return render_template("articles.html", articles=result)
 
 
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
