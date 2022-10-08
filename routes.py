9fdf6d98d2512202d9cecc3dbcb9984c4f032fdc
임태민 <devtaemin@gmail.com>
2022-10-09 00:13:27 +0900

    update routes, main, articles and add 404

--- routes.py
+++ routes.py
@@ -15,6 +15,11 @@ def home():
     return render_template('main.html')
 
 
+@app.route('/404', methods=['GET'])
+def error():
+    return render_template('404.html')
+
+
 @app.route('/articles', methods=['GET'])
 def today():
     if request.method == 'GET':   #데이터를 받아야 아래 코드를 더 짤수 있을듯 하네용.. 우선은 articles로 넘어가짐
